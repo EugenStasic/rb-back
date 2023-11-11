d09d0904274c571bc701717ac6ab5fa453da7bbb
Eugen <stasic.eugen@gmail.com>
2023-11-11 19:34:05 +0100

    Changes to boat model and routes

--- models/Boat.js
+++ models/Boat.js
@@ -1,62 +1,110 @@
 const mongoose = require('mongoose');
 
 const BoatSchema = new mongoose.Schema({
-    ownerId: {
-        type: mongoose.Schema.Types.ObjectId,
-        required: true,
-        ref: 'User'
-    },
-
+  ownerId: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: 'User'
+  },
+  generalInformation: {
     type: {
-        type: String,
-        required: true,
-        enum: ['Motorboat', 'Sailboat', 'RIB', 'Catamaran', 'Jet-Ski', 'Yacht']
+      type: String,
+      required: true,
+      enum: ['Motorboat', 'Sailboat', 'RIB', 'Catamaran', 'Jet-Ski', 'Yacht'],
+      immutable: true
     },
-
     manufacturer: {
-        type: String,
-        required: true,
+      type: String,
+      required: true
     },
-
     model: {
-        type: String,
-        required: true
+      type: String,
+      required: true
     },
-
     cityHarbour: {
+      postalCode: {
         type: String,
-        required: true,
-    },
-
-    skipperOption: {
+        required: true
+      },
+      city: {
         type: String,
-        required: true,
-        enum: ['Yes', 'No', 'Both']
-    },
-
-    capacity: {
-        type: Number,
         required: true
+      }
     },
-
-    length: {
-        type: Number,
-        required: true
+    description: String,
+    photos: [String]
+  },
+  technicalInformation: {
+    boatLength: {
+      type: Number,
+      required: true
     },
-
-    engine: {
-        type: {
-            type: String,
-            required: true,
-            enum: ['Inboard', 'Outboard']
-        },
-
-        power: {
-            type: Number,
-            required: true
-        }
+    onboardCapacity: {
+      type: Number,
+      required: true
+    },
+    engineType: {
+      type: String,
+      required: true,
+      enum: ['Inboard', 'Outboard']
+    },
+    enginePower: {
+      type: Number,
+      required: true
+    },
+    avgFuelConsumption: Number,
+    yearOfConstruction: Number
+  },
+  pricing: {
+    referencePrice: Number,
+    minCharterPeriod: {
+      type: String,
+      enum: ['Half-Day', '1 Day', '2 Days', '3 Days', '4 Days', '5 Days', '6 Days', '7 Days']
+    },
+    pricePeriods: [{
+      fromDate: Date,
+      toDate: Date,
+      price: Number
+    }]
+  },
+  booking: {
+    checkInTime: String,
+    checkOutTime: String,
+    boatLicenseRequirement: {
+      type: String,
+      enum: ['Yes', 'No']
+    },
+    fuelCost: {
+      type: String,
+      enum: ['Included', 'Not included']
+    },
+    cancellationConditions: {
+      type: String,
+      enum: ['Flexible', 'Moderate', 'Strict']
     }
-})
+  },
+  equipment: {
+    navigationEquipment: {
+        type: [String],
+        enum: ['Bow thruster', 'Electric windlass', 'Autopilot', 'GPS', 'Depth sounder', 'VHF', 'Guides & Maps'],
+        default: []
+      },
+      boatEquipment: {
+        type: [String],
+        enum: ['Bimini', 'Shower', 'External table', 'External speakers', 'Teak deck', 'Sundeck', 'Bathing Platform', 'Bathing ladder'],
+        default: []
+      },
+      waterSportsEquipment: {
+        type: [String],
+        enum: ['Water skis', 'Wakeboard', 'Towable Tube'],
+        default: []
+      }
+  },
+  extras: [{
+    option: String,
+    pricePerDay: Number
+  }]
+});
 
 const Boat = mongoose.model('Boat', BoatSchema);
 
--- routes/boat.js
+++ routes/boat.js
@@ -6,28 +6,64 @@ const authenticate = require('../middleware/authenticate');
 const router = express.Router();
 
 router.post('/register', [
-    body('type').notEmpty().isIn(['Motorboat', 'Sailboat', 'RIB', 'Catamaran', 'Jet-Ski', 'Yacht']).withMessage('Type is required'),
-    body('manufacturer').notEmpty().withMessage('Manufacturer is required'),
-    body('model').notEmpty().withMessage('Model is required'),
-    body('cityHarbour').notEmpty().withMessage('City/Harbour is required'),
-    body('skipperOption').notEmpty().isIn(['Yes', 'No', 'Both']).withMessage('Skipper options is required'),
-    body('capacity').notEmpty().withMessage('Maximum capacity of the boat is required'),
-    body('length').notEmpty().withMessage('Boat length is required'),
-    body('engine.type').notEmpty().isIn(['Inboard', 'Outboard']).withMessage('Engine type is required'),
-    body('engine.power').notEmpty().withMessage('Engine power is required')
-
+    body('generalInformation.type')
+        .notEmpty()
+        .isIn(['Motorboat', 'Sailboat', 'RIB', 'Catamaran', 'Jet-Ski', 'Yacht'])
+        .withMessage('Boat type is required'),
+    body('generalInformation.manufacturer')
+        .notEmpty()
+        .withMessage('Manufacturer is required'),
+    body('generalInformation.model')
+        .notEmpty()
+        .withMessage('Model is required'),
+    body('generalInformation.cityHarbour.postalCode')
+        .notEmpty()
+        .withMessage('Postal code is required'),
+    body('generalInformation.cityHarbour.city')
+        .notEmpty()
+        .withMessage('City is required'),
+    body('technicalInformation.boatLength')
+        .notEmpty()
+        .withMessage('Boat length is required'),
+    body('technicalInformation.onboardCapacity')
+        .notEmpty()
+        .withMessage('Onboard capacity is required'),
+    body('technicalInformation.engineType')
+        .notEmpty()
+        .isIn(['Inboard', 'Outboard'])
+        .withMessage('Engine type is required'),
+    body('technicalInformation.enginePower')
+        .notEmpty()
+        .withMessage('Engine power is required'),
 ], authenticate, async (req, res) => {
+
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() });
-    };
+    }
 
     try {
         const ownerId = req.user.userId;
-        const { type, manufacturer, model, cityHarbour, skipperOption, capacity, length, engine } = req.body;
-        const newBoat = new Boat({ ownerId, type, manufacturer, model, cityHarbour, skipperOption, capacity, length, engine });
+        const {
+            generalInformation,
+            technicalInformation,
+            pricing,
+            booking,
+            equipment,
+            extras
+        } = req.body;
+
+        const newBoat = new Boat({
+            ownerId,
+            generalInformation,
+            technicalInformation,
+            pricing,
+            booking,
+            equipment,
+            extras
+        });
+
         await newBoat.save();
-        
         res.status(201).json({ message: 'Boat registered successfully', boat: newBoat });
     } catch (error) {
         res.status(500).json({ message: 'Server error', error: error.message });
@@ -60,52 +96,73 @@ router.get('/:boatId', authenticate, async (req, res) => {
     }
 })
 
-router.patch('/user-boats/:boatId',[
-    body('type').notEmpty().isIn(['Motorboat', 'Sailboat', 'RIB', 'Catamaran', 'Jet-Ski', 'Yacht']).withMessage('Type is required'),
-    body('manufacturer').notEmpty().withMessage('Manufacturer is required'),
-    body('model').notEmpty().withMessage('Model is required'),
-    body('cityHarbour').notEmpty().withMessage('City/Harbour is required'),
-    body('skipperOption').notEmpty().isIn(['Yes', 'No', 'Both']).withMessage('Skipper options is required'),
-    body('capacity').notEmpty().withMessage('Maximum capacity of the boat is required'),
-    body('length').notEmpty().withMessage('Boat length is required'),
-    body('engine.type').notEmpty().isIn(['Inboard', 'Outboard']).withMessage('Engine type is required'),
-    body('engine.power').notEmpty().withMessage('Engine power is required')
-],authenticate, async (req, res) => {
-    console.log("Received PATCH request for boat update");
+router.patch('/user-boats/:boatId', [
+    body('generalInformation.manufacturer').optional().isString().withMessage('Manufacturer must be a string'),
+    body('generalInformation.model').optional().isString().withMessage('Model must be a string'),
+    body('generalInformation.cityHarbour.postalCode').optional().isString().withMessage('Postal code must be a string'),
+    body('generalInformation.cityHarbour.city').optional().isString().withMessage('City must be a string'),
+    body('generalInformation.description').optional().isString().withMessage('Description must be a string'),
+    body('generalInformation.photos.*').optional().isString().withMessage('Each photo must be a string'),
+
+    body('technicalInformation.boatLength').optional().isNumeric().withMessage('Boat length must be a number'),
+    body('technicalInformation.onboardCapacity').optional().isNumeric().withMessage('Onboard capacity must be a number'),
+    body('technicalInformation.engineType').optional().isIn(['Inboard', 'Outboard']).withMessage('Invalid engine type'),
+    body('technicalInformation.enginePower').optional().isNumeric().withMessage('Engine power must be a number'),
+    body('technicalInformation.avgFuelConsumption').optional().isNumeric().withMessage('Average fuel consumption must be a number'),
+    body('technicalInformation.yearOfConstruction').optional().isNumeric().withMessage('Year of construction must be a number'),
+
+    body('pricing.referencePrice').optional().isNumeric().withMessage('Reference price must be a number'),
+    body('pricing.minCharterPeriod').optional().isIn(['Half-Day', '1 Day', '2 Days', '3 Days', '4 Days', '5 Days', '6 Days', '7 Days']).withMessage('Invalid minimum charter period'),
+    body('pricing.pricePeriods.*.fromDate').optional().isDate().withMessage('From date must be a date'),
+    body('pricing.pricePeriods.*.toDate').optional().isDate().withMessage('To date must be a date'),
+    body('pricing.pricePeriods.*.price').optional().isNumeric().withMessage('Price must be a number'),
+
+    body('booking.checkInTime').optional().isString().withMessage('Check-in time must be a string'),
+    body('booking.checkOutTime').optional().isString().withMessage('Check-out time must be a string'),
+    body('booking.boatLicenseRequirement').optional().isIn(['Yes', 'No']).withMessage('Invalid boat license requirement'),
+    body('booking.fuelCost').optional().isIn(['Included', 'Not included']).withMessage('Invalid fuel cost option'),
+    body('booking.cancellationConditions').optional().isIn(['Flexible', 'Moderate', 'Strict']).withMessage('Invalid cancellation condition'),
+
+    body('equipment.navigationEquipment.*').optional().isString().withMessage('Each navigation equipment item must be a string'),
+    body('equipment.boatEquipment.*').optional().isString().withMessage('Each boat equipment item must be a string'),
+    body('equipment.waterSportsEquipment.*').optional().isString().withMessage('Each water sports equipment item must be a string'),
+
+    body('extras.*.option').optional().isString().withMessage('Each extra option must be a string'),
+    body('extras.*.pricePerDay').optional().isNumeric().withMessage('Price per day must be a number'),
+], authenticate, async (req, res) => {
+    const { boatId } = req.params;
+    const updates = req.body;
 
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-        console.error("Validation errors:", errors.array());
         return res.status(400).json({ errors: errors.array() });
-    };
+    }
 
     try {
-        const boatId = req.params.boatId;
-        console.log("Boat ID:", boatId);
-
-        const updates = req.body;
-        console.log("Update data:", updates);
-
         const boat = await Boat.findById(boatId);
         if (!boat) {
-            console.error("Boat not found with ID:", boatId);
             return res.status(404).json({ message: 'Boat not found' });
-        };
-
-        console.log("Boat found, checking ownership...");
+        }
         if (boat.ownerId.toString() !== req.user.userId) {
-            console.error("Unauthorized attempt to edit boat by User ID:", req.user.userId);
             return res.status(403).json({ message: 'User is not authorized to edit boat information' });
-        };
+        }
 
-        console.log("Ownership verified, updating boat...");
-        const updatedBoat = await Boat.findByIdAndUpdate(boatId, { $set: updates }, { new: true, runValidators: true });
-        
-        console.log("Boat updated successfully:", updatedBoat);
-        res.status(200).json({ message: 'Boat information updated successfully', boat: updatedBoat })
+        const updateQuery = {};
+        for (const [key, value] of Object.entries(updates)) {
+            if (value && typeof value === 'object' && !Array.isArray(value)) {
+                for (const [nestedKey, nestedValue] of Object.entries(value)) {
+                    updateQuery[`${key}.${nestedKey}`] = nestedValue;
+                }
+            } else {
+                updateQuery[key] = value;
+            }
+        }
+
+        const updatedBoat = await Boat.findByIdAndUpdate(boatId, { $set: updateQuery }, { new: true, runValidators: true });
+
+        res.status(200).json({ message: 'Boat information updated successfully', boat: updatedBoat });
     } catch (error) {
-        console.error("Server error during boat update:", error.message);
-        res.status(500).json({ message: 'Server error', error: error.message })
+        res.status(500).json({ message: 'Server error', error: error.message });
     }
 });
 
